80c0b3dbb19d6673711e510bbafe5aac14e0f995
myungwook <encarmwkim@gmail.com>
2018-11-13 17:15:12 +0900

    하다보니 자료구조...

--- src/com/java/study/day2/Main.java
+++ src/com/java/study/day2/Main.java
@@ -1,8 +1,8 @@
 package com.java.study.day2;
 
-public class MainClass {
+public class Main {
 	public static void main(String args[]) {
-		MainClass mainClass = new MainClass();
+		Main mainClass = new Main();
 		mainClass.arrayListTest();
 		mainClass.queueTest();
 		mainClass.stackTest();
